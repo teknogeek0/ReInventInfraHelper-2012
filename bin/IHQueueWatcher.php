cf31d525c92e01133bcc092328e936ab0eb04896
Chris Munns <chrismunns@gmail.com>
2012-11-17 13:51:37 -0500

    test queuewatcher lib linking and variable pass in so far

---
+++ bin/IHQueueWatcher.php
@@ -0,0 +1,11 @@
+<?php
+  require_once 'AWSSDKforPHP/sdk.class.php';
+  require_once 'IHResources.php';
+
+  $sqs = new AmazonSQS();
+  $response = $sqs->get_queue_size($IHQueue);
+
+  // Success?
+  :var_dump($response);
+
+?>
