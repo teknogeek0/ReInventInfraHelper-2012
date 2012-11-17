e76a802a1d30a41e7d3523d8ced8269379798e75
Chris Munns <chrismunns@gmail.com>
2012-11-17 13:44:55 -0500

    initial checkin of infrahelper stuff

---
+++ bin/IHResources.php
@@ -0,0 +1,5 @@
+<?php
+  $IHQueue="https://sqs.us-east-1.amazonaws.com/102901597367/infrahelper04-InfraHelperQueue-58TQ5AKFHN93";
+  $IHSWFDomain="Infrahelper";
+
+?>
---
+++ config.inc.php
@@ -0,0 +1,11 @@
+<?php if (!class_exists('CFRuntime')) die('No direct access allowed.');
+
+CFCredentials::set(array(
+	'development' => array(
+		'key' => 'AKIAIEFRMTR6AJ2T4DMQ',
+		'secret' => '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',
+		'default_cache_config' => '',
+		'certificate_authority' => false
+	),
+	'@default' => 'development'
+));
