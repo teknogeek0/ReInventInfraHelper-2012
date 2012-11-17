1cff4385a6934d1673ea4939c3059bb5f83e351d
Chris Munns <chrismunns@gmail.com>
2012-11-17 16:30:03 -0500

    adding in more structure to our queue watcher

--- bin/IHQueueWatcher.php
+++ bin/IHQueueWatcher.php
@@ -5,44 +5,69 @@
   $sqs = new AmazonSQS();
   $response = $sqs->receive_message($IHQueue);
 
-  // Success?
-  $body = $response->body->ReceiveMessageResult->Message[0]->Body;
-  $msg_id=$response->body->ReceiveMessageResult->Message[0]->MessageId;
-  $rcpt_hand=($response->body->ReceiveMessageResult->Message[0]->ReceiptHandle);
+  if ($response->isOK()) 
+  {
+	  // Success?
+	  $body = $response->body->ReceiveMessageResult->Message[0]->Body;
+	  $msg_id=$response->body->ReceiveMessageResult->Message[0]->MessageId;
+	  $rcpt_hand=($response->body->ReceiveMessageResult->Message[0]->ReceiptHandle);
 
-  $msg_body = json_decode($response->body->ReceiveMessageResult->Message->Body, TRUE); 
-  $message_attrs = json_decode($msg_body["Message"], TRUE);
-  #print_r($message_attrs);
-  echo "";
-  
-  $eventType = $message_attrs["Event"];
-  
-  if ( $eventType == "autoscaling:EC2_INSTANCE_LAUNCH" )
-  {
-   $instanceID = $message_attrs["EC2InstanceId"];
-   $ASGroupName = $message_attrs["AutoScalingGroupName"];
-   $StartTime = $message_attrs["StartTime"];
-   $EndTime = $message_attrs["EndTime"];
-  }
-  elseif ( $eventType == "autoscaling:EC2_INSTANCE_LAUNCH_ERROR" )
-  {
-  }
-  elseif ( $eventType == "autoscaling:EC2_INSTANCE_TERMINATE" )
-  {
-  }
-  elseif ( $eventType == "autoscaling:EC2_INSTANCE_TERMINATE_ERROR" )
-  {
-  }
-  elseif ( $eventType == "autoscaling:TEST_NOTIFICATION" )
-  {
-   ## can ignore this, won't be doing anything
-   echo "just a test";
-   exit;
+	  $msg_body = json_decode($response->body->ReceiveMessageResult->Message->Body, TRUE); 
+	  $message_attrs = json_decode($msg_body["Message"], TRUE);
+	  #print_r($message_attrs);
+	 
+	  $eventType = $message_attrs["Event"];
+	  
+	  if (preg_match("/autoscaling:.*/", $eventType)
+    {
+		  if ( $eventType != "autoscaling:TEST_NOTIFICATION" )
+		  {
+				$instanceID = $message_attrs["EC2InstanceId"];
+				$ASGroupName = $message_attrs["AutoScalingGroupName"];
+				$StartTime = $message_attrs["StartTime"];
+				$EndTime = $message_attrs["EndTime"];
+
+				if ( $eventType == "autoscaling:EC2_INSTANCE_LAUNCH" )
+				{
+					echo "this is a launch of a new instance";
+				}
+				elseif ( $eventType == "autoscaling:EC2_INSTANCE_LAUNCH_ERROR" )
+				{
+					echo "this is a failed launch of a new instance";
+				}
+				elseif ( $eventType == "autoscaling:EC2_INSTANCE_TERMINATE" )
+				{
+					echo "this is an instance termination";
+				}
+				elseif ( $eventType == "autoscaling:EC2_INSTANCE_TERMINATE_ERROR" )
+				{
+					echo "this is an error of a terminate instance";
+				}
+				else
+				{
+					echo "something go boom\n";
+	        print_r($message_attrs);
+	        exit;
+				}
+		  }
+		  elseif ( $eventType == "autoscaling:TEST_NOTIFICATION" )
+		  {
+		   ## can ignore this, won't be doing anything
+		   echo "just a test";
+		   exit;
+		  }
+	  }
+	  else
+	  {
+	    echo "something go boom\n";
+	    print_r($message_attrs);
+	    exit;
+	  }
   }
   else
   {
-   echo "something go boom\n";
-   print_r($message_attrs);
+  	var_dump($response);
+  	exit;
   }
 
 ?>
