288ac4bcf1e02e6c07de987f6d864d5d744a9236
Chris Munns <chrismunns@gmail.com>
2012-11-17 23:51:24 -0500

    more structure and sanity to creating a workflowtype

--- bin/IHSWFsetup.php
+++ bin/IHSWFsetup.php
@@ -9,43 +9,60 @@
 	// Setup
 	$workflow_domain = $IHSWFDomain;
 	$workflow_type_name = "IHWorkFlowMain";
-	 
-	//-----------------------------------------------------------------//
-	// Register a new workflow type
-	 
-	echo '# Registering a new workflow type...' . PHP_EOL;
-	$workflow_type = $swf->register_workflow_type(array(
-	    'domain'             => $workflow_domain,
-	    'name'               => $workflow_type_name,
-	    'version'            => '1.0',
-	    'description'        => 'A test task to show how this thing works.',
-	    'defaultChildPolicy' => AmazonSWF::POLICY_TERMINATE
-	));
-	 
-	if ($domain->isOK())
+
+	function makeworkflowtype()
 	{
-	    echo 'Waiting for the workflow type to become ready...' . PHP_EOL;
-	 
-	    do {
-	        sleep(1);
-	 
-	        $describe = $swf->describe_workflow_type(array(
-	            'domain'       => $workflow_domain,
-	            'workflowType' => array(
-	                'name'    => $workflow_type_name,
-	                'version' => '1.0'
-	            )
-	        ));
-	    }
-	    while ((string) $describe->body->typeInfo->status !== AmazonSWF::STATUS_REGISTERED);
-	 
-	    echo 'Worktype flow was created successfully.' . PHP_EOL;
-	}
-	else
-	{
-	    echo "Workflow type creation failed." . PHP_EOL;
-	}
-	 
-	echo PHP_EOL;
+		$describe = $swf->describe_workflow_type(array(
+	    'domain'       => $workflow_domain,
+	    'workflowType' => array(
+	        'name'    => $workflow_type_name,
+	        'version' => '1.0'
+	    )
+	  ));
+
+	  if (isset($describe->body->typeInfo) && ($describe->body->typeInfo->to_array() == "REGISTERED"))
+	  {
+	    echo "The workflow exists, so move on to creating the Activities" . PHP_EOL;
+	  }
+	  else
+	  {
+			##Register a new workflow type
+			echo '# Registering a new workflow type...' . PHP_EOL;
+			$workflow_type = $swf->register_workflow_type(array(
+		    'domain'             => $workflow_domain,
+		    'name'               => $workflow_type_name,
+		    'version'            => '1.0',
+		    'description'        => 'A test task to show how this thing works.',
+		    'defaultChildPolicy' => AmazonSWF::POLICY_TERMINATE
+			));
+			 
+			if ($workflow_type->isOK())
+			{
+			    echo 'Waiting for the workflow type to become ready...' . PHP_EOL;
+			 
+			    do {
+			        sleep(1);
+			 
+			        $describe = $swf->describe_workflow_type(array(
+			            'domain'       => $workflow_domain,
+			            'workflowType' => array(
+		                'name'    => $workflow_type_name,
+		                'version' => '1.0'
+			            )
+			        ));
+			    }
+			    while ((string) $describe->body->typeInfo->status !== AmazonSWF::STATUS_REGISTERED);
+			 
+			    echo 'Worktype flow was created successfully.' . PHP_EOL;
+			}
+			else
+			{
+			  echo "Workflow type creation failed." . PHP_EOL;
+			}
+		} 
+  }
+
+##run makeworkflowtype
+makeworkflowtype();
 
 ?>
