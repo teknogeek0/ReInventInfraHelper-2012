69d068575056d7efff8bb324f1f568f5a4df85ac
Chris Munns <chrismunns@gmail.com>
2012-11-18 01:09:22 -0500

    gonna need this change first

--- bin/IHResources.php
+++ bin/IHResources.php
@@ -1,5 +1,5 @@
 <?php
   $IHQueue="https://sqs.us-east-1.amazonaws.com/102901597367/infrahelper04-InfraHelperQueue-58TQ5AKFHN93";
-  $IHSWFDomain="Infrahelper";
+  $IHSWFDomain="Infrahelper2";
 
 ?>
