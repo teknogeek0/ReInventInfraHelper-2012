77930ba454a4806d96bd6a63f846ca6d1ee75d20
Chris Munns <chrismunns@gmail.com>
2012-11-18 01:13:02 -0500

    lil refactor on make activities flow

--- bin/IHSWFsetup.php
+++ bin/IHSWFsetup.php
@@ -26,10 +26,7 @@
 		  if ($MyStatus == "REGISTERED")
 		  {
 		    echo "The workflow exists, so move on to creating the Activities" . PHP_EOL;
-		    MakeActivity($swf, $workflow_domain, $workflow_type_name, "EIPMapper", "Maps EIPs to Instances");
-		    MakeActivity($swf, $workflow_domain, $workflow_type_name, "VPCRouteMapper", "Map routes in a VPC due to an instance change");
-		    MakeActivity($swf, $workflow_domain, $workflow_type_name, "ChefRemoveClientNode", "Remove Chef nodes and clients in response to an instance no longer existing");
-		    echo "All done with creating the WorkFlow and Activity Types" . PHP_EOL;
+		    DoMakeActivities($swf, $workflow_domain, $workflow_type_name);
 		  }
 		}
 	  else
@@ -64,7 +61,8 @@
 			    }
 			    while ((string) $describe->body->typeInfo->status !== AmazonSWF::STATUS_REGISTERED);
 			 
-			    echo "Workflow type $workflow_type_name was created successfully." . PHP_EOL;
+			    echo "Workflow type $workflow_type_name was created successfully. Now lets make Activities." . PHP_EOL;
+			    DoMakeActivities($swf, $workflow_domain, $workflow_type_name);
 
 			}
 			else
@@ -133,6 +131,14 @@
 			    echo "Activity type $activity_type_name creation failed.";
 			}
 	  }
+  }
+
+  function DoMakeActivities($swf, $workflow_domain, $workflow_type_name)
+  {
+  	MakeActivity($swf, $workflow_domain, $workflow_type_name, "EIPMapper", "Maps EIPs to Instances");
+		MakeActivity($swf, $workflow_domain, $workflow_type_name, "VPCRouteMapper", "Map routes in a VPC due to an instance change");
+		MakeActivity($swf, $workflow_domain, $workflow_type_name, "ChefRemoveClientNode", "Remove Chef nodes and clients in response to an instance no longer existing");
+		echo "All done with creating the WorkFlow and Activity Types" . PHP_EOL;
   }
 
 
